4bdfe4fa10f303579fa7685cfd2ff9559c69a1d7
kristinaerm <32340053+kristinaerm@users.noreply.github.com>
2017-12-10 15:55:57 +0400

    прппп

---
+++ ServerMenager/src/model/TaskLog.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import exceptions.InvalidRecordFieldException;
+//import view.SimpleTaskManager;
+import java.util.LinkedList;
+import java.util.UUID;
+import javax.swing.JTable;
+import org.w3c.dom.Document;
+import view.Transfer;
+
+/**
+ *
+ * @author USER
+ */
+public class TaskLog {
+
+    private static final String[] COLUMN_NAMES = {"№", "Название", "Время и дата", "Описание", "Контакты"};
+    private LinkedList<Record> records;
+    private final String id;
+
+    public TaskLog(LinkedList<Record> rec) {
+
+        id = UUID.randomUUID().toString();
+        records = rec;
+        sort();
+
+    }
+
+    public void updateTable() {
+
+        while (Transfer.model.getRowCount() != 0) {
+            for (int i = 0; i < Transfer.model.getRowCount(); i++) {
+                Transfer.model.removeRow(i);
+            }
+        }
+        for (int i = 0; i < records.size(); i++) {
+            Transfer.model.addRow(new Object[]{i, getRecord(i).getName(), getRecord(i).getTimeString(), getRecord(i).getDescription(), getRecord(i).getContacts()});
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LinkedList<Record> getRecords() {
+        return records;
+    }
+
+    public int getNumberOfRecords() {
+        return records.size();
+    }
+
+    public Object[][] createData() {
+        Object[][] data = new Object[records.size()][5];
+        for (int i = 0; i < records.size(); i++) {
+            data[i][0] = i;
+            data[i][1] = this.getRecord(i).getName();
+            data[i][2] = this.getRecord(i).getTimeString();
+            data[i][3] = this.getRecord(i).getDescription();
+            data[i][4] = this.getRecord(i).getContacts();
+        }
+        return data;
+    }
+
+    public void changeRecord(int n, String na, String ti, String des, String con) throws InvalidRecordFieldException {
+        records.get(n);
+        if ((!na.equals(""))) {
+            records.get(n).setName(na);
+        }
+        if ((!ti.equals(""))) {
+            records.get(n).setTime(ti);
+        }
+        if ((!des.equals(""))) {
+            records.get(n).setDescription(des);
+        }
+        if ((!con.equals(""))) {
+            records.get(n).setContacts(con);
+        }
+        sort();
+        updateTable();
+    }
+
+    public Record getRecord(int n) {
+
+        return records.get(n);
+    }
+
+    public void addRecord(Record rec) {
+        records.add(rec);
+        sort();
+        updateTable();
+    }
+
+    public void deleteRecord(int n) {
+        records.remove(n);
+
+        updateTable();
+    }
+
+    public void sort() {
+        Record temp = null;
+
+        for (int j = 0; j < records.size(); j++) {
+            for (int k = 0; k < records.size() - 1; k++) {
+                if (records.get(k).compareTo(records.get(k + 1)) == 1) {
+                    temp = records.get(k);
+                    records.set(k, records.get(k + 1));
+                    records.set(k + 1, temp);
+                }
+            }
+        }
+    }
+}
